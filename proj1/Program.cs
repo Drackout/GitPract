7d603a6a827e27a0d4caaf3729b5262becb1f8cb
Drackout <mikatx2@hotmail.com>
2025-05-01 21:31:02 +0100

    Add function

---
+++ proj1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace proj1
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}
