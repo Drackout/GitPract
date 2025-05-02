558b40d90de07daf90b95caa86714bf5f65f17c3
Drackout <mikatx2@hotmail.com>
2025-05-02 01:14:14 +0100

    First commit

--- proj1/Program.cs
+++ proj1/Program.cs
@@ -6,7 +6,7 @@ namespace proj1
     {
         private static void Main(string[] args)
         {
-            ////A
+            ////
             ////B
             Console.WriteLine("Hello LP1!");
         }
