eb6ede977f521530c01d948d99f2912ffbc72eb0
Drackout <mikatx2@hotmail.com>
2025-05-02 01:15:45 +0100

    Fix bug

--- proj1/Program.cs
+++ proj1/Program.cs
@@ -7,7 +7,7 @@ namespace proj1
         private static void Main(string[] args)
         {
             ///
-            ////B
+            ////
             Console.WriteLine("Hello LP1!");
         }
     }
